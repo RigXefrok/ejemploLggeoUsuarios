1f551282e6f9d1333bf23a4d0748a0f8e23e81e0
sebastian de la Fuente <sebas@MacBook-Air-de-sebastian.local>
2025-07-20 22:02:07 -0300

    Clase 2

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    static final String usuarioCorrecto = "sebadlf";
-    static final String contrasenaCorrrecta = "adminadmin";
-
     public static void main(String[] args) {
-        short cantidadDeIntentos = 3;
-        Scanner scanner = new Scanner(System.in);
-
-        while (cantidadDeIntentos > 0) {
-            System.out.print("Ingrese su usuario: ");
-            String usuario = scanner.nextLine();
-            System.out.print("Ingrese su contraseña: ");
-            String contrasena = scanner.nextLine();
-
-            if (usuario.equals(usuarioCorrecto) && contrasena.equals(contrasenaCorrrecta))  {
-                System.out.println("Bienvenido " + usuario);
-                cantidadDeIntentos = 0;
-            } else {
-                System.out.println("El usuario o la contraseña son incorrectos");
-                cantidadDeIntentos -= 1;
-                if (cantidadDeIntentos == 0) {
-                    System.out.println("Se agotaron la cantidad de intentos");
-                }
-            }
-        }
-        scanner.close();
+        Auto miAuto = new Auto();
+        System.out.println(miAuto.estasEnMarcha());
+        miAuto.poneteEnMarcha();
+        System.out.println(miAuto.estasEnMarcha());
+        System.out.println(miAuto.cuantaNaftaTenes());
+        miAuto.cargarNafta(90);
+        System.out.println(miAuto.cuantaNaftaTenes());
     }
 }
